bf06363f0b91197c51b73d553c42d84729ed8205
Saturne <alexandrethomas180@gmail.com>
2024-11-17 18:25:31 +0100

    ajout entreprise

---
+++ src/Form/EntrepriseType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Entreprise;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EntrepriseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('poste_entreprise')
+            ->add('fiche_entreprise')
+            ->add('ref_utilisateur', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Entreprise::class,
+        ]);
+    }
+}
